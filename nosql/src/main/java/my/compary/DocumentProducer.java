8fce94c8a50cba6866d0c4008ee19f849c7e02d2
otaviojava <otaviopolianasantana@gmail.com>
2020-04-03 19:10:35 -0300

    creates properties

--- nosql/src/main/java/my/compary/DocumentProducer.java
+++ nosql/src/main/java/my/compary/DocumentProducer.java
@@ -4,30 +4,29 @@ import com.mongodb.MongoClient;
 import jakarta.nosql.document.DocumentCollectionManager;
 import org.eclipse.jnosql.diana.mongodb.document.MongoDBDocumentCollectionManagerFactory;
 import org.eclipse.jnosql.diana.mongodb.document.MongoDBDocumentConfiguration;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import sh.platform.config.Config;
 import sh.platform.config.MongoDB;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class DocumentProducer {
 
 
+    @Inject
+    @ConfigProperty(name = "document")
+    private DocumentCollectionManager manager;
+
     @Produces
-    @ApplicationScoped
-    public DocumentCollectionManager getDocumentCollectionManager() {
-        Config config = new Config();
-        final MongoDB mongo = config.getCredential("mongodb", MongoDB::new);
-        final MongoClient mongoClient = mongo.get();
-        MongoDBDocumentConfiguration configuration = new MongoDBDocumentConfiguration();
-        MongoDBDocumentCollectionManagerFactory factory = configuration.get(mongoClient);
-        return factory.get(mongo.getDatabase());
+    public DocumentCollectionManager getManager() {
+        return manager;
     }
 
-    public void close(@Disposes DocumentCollectionManager manager) {
+    public void destroy(@Disposes DocumentCollectionManager manager) {
         manager.close();
     }
-
 }
